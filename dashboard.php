2f28324b6eb300cc557149b37e6e49ca1bb6db24
Priandana Gerdian <thepriandana@gmail.com>
2020-03-29 00:11:27 +0900

    Dashboard base

---
+++ dashboard.php
@@ -0,0 +1,61 @@
+<?php   
+session_start();
+if(!isset($_SESSION['id_applicant'])){
+  header("Location: login.php");
+}else if(isset($_SESSION['id_officer'])){
+  header("Location: officer/dashboard.php");
+  die();
+}else{
+  require_once("main.php");
+  $sql = new SQL();
+  $bind[':id'] = $_SESSION['id_user'];
+  $bio = $sql->run("SELECT fullname as name FROM user WHERE userID = :id", $bind);
+  die('<!DOCTYPE html>
+  <html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Dashboard | Kosan</title>
+    <link rel="icon" href="image/icon.svg">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/kosan.css">
+    <link href="fontawesome-free-5.11.2-web/css/all.min.css" rel="stylesheet">
+    <script src="js/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <div class="container mt-5">
+      <div class="col-md-10 border shadow-sm py-3 mx-auto row">
+        <div class="col-md-1 border-right p-0">
+          <ul class="nav d-md-flex">
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.php" alt="Dashboard"><span class="l l-home l-active"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="residence.php" alt="Residence"><span class="l l-residence"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="application.php" alt="Application"><span class="l l-application"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="profile.php" alt="Profile"><span class="l l-profile"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php" alt="Log out"><span class="l l-logout"></span></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-11 pr-md-0">
+          <p class="text-right">'.ucwords($bio[0]['name']).'</p>
+          <div class="content p-2">
+          </div>
+          <div class="bottom bg-secondary p-2 rounded">
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+  </html>
+');
+}
+?>
