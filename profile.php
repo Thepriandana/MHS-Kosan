dcd472c469d3b6fd452d2fbb5750723ce1e2a3d5
Thepriandana <19623334+Thepriandana@users.noreply.github.com>
2020-04-26 21:22:47 +0900

    Fix officer profile design and typo code

--- profile.php
+++ profile.php
@@ -1,29 +1,27 @@
 <?php
 session_start();
-if(!isset($_SESSION['id_applicant'])){
+if(!isset($_SESSION['id_officer'])){
   header("Location: login.php");
-}else if(isset($_SESSION['id_officer'])){
-  header("Location: officer/dashboard.php");
+}else if(isset($_SESSION['id_applicant'])){
+  header("Location: ../dashboard.php");
   die();
 }else if(!empty($_POST)){
-  if(isset($_POST['name'], $_POST['email'], $_POST['mi'])){
-    require_once("main.php");
+  if(isset($_POST['name'])){
+    require_once("../main.php");
     $sql = new SQL();
     $bind[':id'] = $_SESSION['id_user'];
     $bind[':n'] = $_POST['name'];
-    $bind[':email'] = strtolower($_POST['email']);
-    $bind[':mi'] = $_POST['mi'];
-    $sql->run("UPDATE user SET fullname = :n WHERE userID = :id; UPDATE applicant SET email = :email, monthlyIncome = :mi WHERE userID = :id", $bind);
+    $sql->run("UPDATE user SET fullname = :n WHERE userID = :id;", $bind);
   }else{
     $_SESSION['error'] = "empty";
   }
   header("Location: profile.php");
   die();
 }else{
-  require_once("main.php");
+  require_once("../main.php");
   $sql = new SQL();
   $bind[':id'] = $_SESSION['id_user'];
-  $bio = $sql->run("SELECT u.fullname as name, a.email, a.monthlyIncome as mi FROM user AS u INNER JOIN applicant AS a ON a.userID = u.userID WHERE u.userID = :id", $bind);
+  $bio = $sql->run("SELECT u.fullname as name FROM user AS u WHERE u.userID = :id", $bind);
   $error = '';
   if(isset($_SESSION['error'])){
     if($_SESSION['error'] === "empty"){
@@ -39,20 +37,20 @@ if(!isset($_SESSION['id_applicant'])){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Dashboard | Kosan</title>
-    <link rel="icon" href="images/logo_kosan.ico">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/kosan.css">
-    <link href="fontawesome-free-5.11.2-web/css/all.min.css" rel="stylesheet">
-    <script src="js/jquery-3.4.1.min.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <link rel="icon" href="image/icon.svg">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/kosan.css">
+    <link href="../fontawesome-free-5.11.2-web/css/all.min.css" rel="stylesheet">
+    <script src="../js/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <script src="../js/bootstrap.min.js" type="text/javascript"></script>
   </head>
   <body>
     <div class="container mt-5">
-      <div class="col-md-10 rounded border shadow-sm py-3 mx-auto row px-0 bg-white">
+    <div class="col-md-10 rounded border shadow-sm py-3 mx-auto row px-0 bg-white">
         <div class="col-auto border-right p-0">
-          <ul class="nav d-block">
+          <ul class="nav" style="display: block;">
             <li class="nav-item">
-              <a class="nav-link l" href="dashboard.php" alt="Dashboard" style="/*! border-bottom: 1px solid; */">
+              <a class="nav-link l" href="dashboard.php" alt="Dashboard">
                 <span class="fa fa-home"></span>
                 <small>Home</small>
               </a>
@@ -84,19 +82,19 @@ if(!isset($_SESSION['id_applicant'])){
           </ul>
         </div>
         <div class="col">
-          <div class="row">
+            <div class="row">
             <div class="col">
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb ml-2 bg-white px-3 py-2 border mb-3">
-                  <li class="breadcrumb-item active" aria-current="page"><a href="profile.php">Profile</a></li>
-                </ol>
-              </nav>
-            </div>
-            <div class="col">
-              <p class="text-right">'.ucwords($bio[0]['name']).'</p>
-            </div>
-          </div>
-          <div class="content p-2">
+                  <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb ml-2 bg-white px-3 py-2 border mb-3">
+                      <li class="breadcrumb-item active" aria-current="page"><a href="profile.php">Profile</a></li>
+                    </ol>
+                  </nav>
+                </div>
+                <div class="col">
+                  <p class="text-right">'.ucwords($bio[0]['name']).'</p>
+                </div>
+              </div>
+          <div class="content p-2 mt-3">
           <p>Enter new data and save to update your profile.</p>
             <div class="col-md-5 p-0">
               <form method="POST">
@@ -104,17 +102,9 @@ if(!isset($_SESSION['id_applicant'])){
                   <label>Full Name</label>
                   <input type="text" class="form-control" name="name" placeholder="'.ucwords($bio[0]['name']).'" value="'.ucwords($bio[0]['name']).'" required>
                 </div>
-                <div class="form-group">
-                  <label>Email</label>
-                  <input type="email" class="form-control" name="email" placeholder="'.$bio[0]['email'].'" value="'.$bio[0]['email'].'" required>
-                </div>
-                <div class="form-group">
-                  <label>Monthly Income</label>
-                  <input type="number" min="10" max="300" class="form-control" name="mi" placeholder="'.$bio[0]['mi'].'" value="'.$bio[0]['mi'].'" required>
-                </div>
                 <div class="d-flex">
                   <a href="password.php" class="d-flex ml-auto mr-2 card-link"><button type="button" class="btn btn-sm btn-danger">Change Password</button></a>
-                  <button type-"submit" class="btn btn-sm btn-primary">Save Profile</button>
+                  <button type="submit" class="btn btn-sm btn-primary">Save Profile</button>
                 </div>
               </form>
             </div>
